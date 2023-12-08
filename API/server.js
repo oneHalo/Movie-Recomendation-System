99d982e2917bd284381cd480a77425748a3a0f2d
Platty114 <jamesplatt02@gmail.com>
2023-12-08 14:35:31 -0700

    create crontoller files, fix tables so they are runnable, add connection to db

--- API/server.js
+++ API/server.js
@@ -10,6 +10,7 @@ import { actorRouter } from "./routes/actor.js";
 import { companyRouter } from "./routes/company.js";
 import { tvShowRouter } from "./routes/shows/tvShow.js";
 import { movieRouter } from "./routes/shows/movie.js";
+import { db } from "./conector/db.js";
 
 //boiler plate code
 dotenv.config()
@@ -17,11 +18,20 @@ const port = process.env.PORT;
 const app = express();
 console.log(`Running on port number ${port}`)
 
+//testing out db connection SUCCESS!
+// const q = "SELECT * FROM UserClient";
 
-
+// db.query(q, (err, data) => {
+//     if(err){
+//         console.log("error connecting to db");
+//     }
+//     else {
+//         console.log(data);
+//     }
+// })
 
 //user router
-app.use("/user", userRouter);
+app.use("/users", userRouter);
 
 //admin router
 app.use("/admin", adminRouter);
